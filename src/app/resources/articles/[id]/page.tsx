3ee9277528bb160152f819bdd760da9e50c07ee7
Abiodun Vlad <omonijoabiodun@gmail.com>
2025-05-09 16:26:41 +0100

    Making [id]/page a module

--- src/app/resources/articles/[id]/page.tsx
+++ src/app/resources/articles/[id]/page.tsx
@@ -35,49 +35,49 @@
 //   return articles.find((article) => article.id === id);
 // }
 
-// export async function generateStaticParams() {
-//   const articles = await getAllArticles();
+export async function generateStaticParams() {
+  //   const articles = await getAllArticles();
 
-//   return articles.map((article) => ({
-//     id: article.id,
-//   }));
-// }
+  //   return articles.map((article) => ({
+  //     id: article.id,
+  //   }));
+  // }
 
-// export default async function ArticlePage({
-//   params,
-// }: {
-//   params: { id: string };
-// }) {
-//   const { id } = params;
+  // export default async function ArticlePage({
+  //   params,
+  // }: {
+  //   params: { id: string };
+  // }) {
+  //   const { id } = params;
 
-//   let article;
-//   try {
-//     article = await fetchArticles(id);
-//   } catch (error) {
-//     console.error("Fetch error:", error);
-//     return <p className="text-black">Error loading article.</p>;
-//   }
+  //   let article;
+  //   try {
+  //     article = await fetchArticles(id);
+  //   } catch (error) {
+  //     console.error("Fetch error:", error);
+  //     return <p className="text-black">Error loading article.</p>;
+  //   }
 
-//   if (!article) {
-//     return <p className="text-black">Article not found.</p>;
-//   }
+  //   if (!article) {
+  //     return <p className="text-black">Article not found.</p>;
+  //   }
 
-//   return (
-//     <div className="max-w-screen-lg mx-auto p-5">
-//       <img
-//         src={article.image}
-//         alt={article.title}
-//         className="w-full h-64 object-cover rounded-lg mb-5"
-//       />
-//       <h1 className="text-3xl text-black font-bold">{article.title}</h1>
-//       <p className="text-sm text-gray-600">
-//         By {article.author} on {article.date}
-//       </p>
+  return (
+    <div className="max-w-screen-lg mx-auto p-5">
+      {/* <img
+        src={article.image}
+        alt={article.title}
+        className="w-full h-64 object-cover rounded-lg mb-5"
+      />
+      <h1 className="text-3xl text-black font-bold">{article.title}</h1>
+      <p className="text-sm text-gray-600">
+        By {article.author} on {article.date}
+      </p>
 
-//       <h2 className="text-xl text-black font-semibold mt-4">
-//         {article.subHeading}
-//       </h2>
-//       <p className="text-gray-700 mt-4">{article.body}</p>
-//     </div>
-//   );
-// }
+      <h2 className="text-xl text-black font-semibold mt-4">
+        {article.subHeading}
+      </h2>
+      <p className="text-gray-700 mt-4">{article.body}</p> */}
+    </div>
+  );
+}
